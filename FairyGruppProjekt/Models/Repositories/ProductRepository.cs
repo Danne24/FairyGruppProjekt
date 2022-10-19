12143305124cf38edad49f02bc8f619a12c69589
Fanniepersson <fannie.persson@hotmail.com>
2022-10-19 12:51:06 +0200

    Added Product,repository,controller and view

--- FairyGruppProjekt/Models/Repositories/ProductRepository.cs
+++ FairyGruppProjekt/Models/Repositories/ProductRepository.cs
@@ -4,53 +4,81 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FairyGruppProjekt.Models.Repositories
 {
-    public class ProductRepository : IRepository<Product>
+    public class ProductRepository : IProductRepository
     {
         private readonly ApplicationDbContext _context;
+
         public ProductRepository(ApplicationDbContext context)
         {
             _context = context;
         }
-        public void Add(Product entity)
-        {
-            _context.Products.Add(entity);
 
-            _context.SaveChanges();
+        public IEnumerable<Product> GetAllProducts
+        {
+            get
+            {
+                return _context.Products.Include(p => p.Category);
+            }
         }
 
-        public void Delete(int id)
+        public IEnumerable<Product> GetProductOnSale
         {
-            var product = _context.Products.FirstOrDefault(p => p.productId == id);
-            _context.Products.Remove(product);
-            _context.SaveChanges();
+            get
+            {
+                return _context.Products.Include(p => p.Category).Where(p => p.IsOnSale);
+            }
         }
 
-        public Product Get(int id)
+        public Product GetProductById(int productId)
         {
-            var product = _context.Products.FirstOrDefault(p => p.productId == id);
-           
-            return product;
-        }
-
-        public IEnumerable<Product> GetAll()
-        {
-            return _context.Products.ToList();
-        }
-
-        public void Save()
-        {
-            _context.SaveChanges();
-        }
-
-        public async Task SaveAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-
-        public void Update(Product entity)
-        {
-            _context.Entry(entity).State = EntityState.Modified;
-            _context.SaveChanges();
+            return _context.Products.FirstOrDefault(p => p.ProductId == productId);
         }
     }
+
+  
+
+
+
+    //public void Add(Product entity)
+    //{
+    //    _context.Products.Add(entity);
+
+    //    _context.SaveChanges();
+    //}
+
+    //public void Delete(int id)
+    //{
+    //    var product = _context.Products.FirstOrDefault(p => p.productId == id);
+    //    _context.Products.Remove(product);
+    //    _context.SaveChanges();
+    //}
+
+    //public Product Get(int id)
+    //{
+    //    var product = _context.Products.FirstOrDefault(p => p.productId == id);
+
+    //    return product;
+    //}
+
+    //public IEnumerable<Product> GetAll()
+    //{
+    //    return _context.Products.ToList();
+    //}
+
+    //public void Save()
+    //{
+    //    _context.SaveChanges();
+    //}
+
+    //public async Task SaveAsync()
+    //{
+    //    await _context.SaveChangesAsync();
+    //}
+
+    //public void Update(Product entity)
+    //{
+    //    _context.Entry(entity).State = EntityState.Modified;
+    //    _context.SaveChanges();
+    //}
+
 }
