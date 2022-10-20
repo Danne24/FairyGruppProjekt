309e8c5c3cf235d74c807c9d9846aa92121e055d
Eddod <edwinwesterberg44@gmail.com>
2022-10-20 13:26:02 +0200

    wwwrootfile and areas updated

--- FairyGruppProjekt/Program.cs
+++ FairyGruppProjekt/Program.cs
@@ -13,7 +13,10 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
